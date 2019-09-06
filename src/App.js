a8c6bc1af719359017d563ec0e59878d62e006d1
Aubbakr <realchampiontheno1@gmail.com>
2019-09-06 16:02:56 +0500

    Added HomePage with smaller Components, Styled MenuItem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import HomePage from './pages/HomePage/HomePage';
+
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Crown Clothing</h1>
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './MenuItem.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className='content'>
+      <h2 className='title'>{title}</h2>
+      <span className='subtitle'>Shop now</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
